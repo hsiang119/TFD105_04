0457d7265efcd9dceb8d700457439b51a47c077f
hsiang119 <g8367ay36@gmail.com>
2022-02-09 19:55:12 +0800

    讓頁尾連結開新分頁

---
+++ js/base.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// footer點連結開新分頁
+// var mywindow;
+
+// var btn = document.getElementsByClassName("fab fa-facebook");
+// btn.addEventListener("click", function(){
+  
+//   mywindow = window.open("https://m.facebook.com/HARNs-Sweets-100358675454637/");
+  
+// });
